8ef868dda057f83e538c0dd56cca40355b7fd813
v.haritonskiy <vharitonsky@gmail.com>
2024-10-31 16:38:59 +0200

    adapt for latest flake

--- flake8_linenumber.py
+++ flake8_linenumber.py
@@ -1,14 +1,96 @@
+import re
+import functools
+import tokenize
+import collections
+from typing import Optional, Dict, Set
 from enum import Enum
 from pathlib import Path
-from flake8.utils import parse_unified_diff
 
 class LineNumberErrors(Enum):
     L001 = 'L001 File is too long (limit: {limit}, total lines: {total_lines})'
+
+DIFF_HUNK_REGEXP = re.compile(r"^@@ -\d+(?:,\d+)? \+(\d+)(?:,(\d+))? @@.*$")
+
+@functools.lru_cache(maxsize=1)
+def stdin_get_value() -> str:
+    """Get and cache it so plugins can use it."""
+    stdin_value = sys.stdin.buffer.read()
+    fd = io.BytesIO(stdin_value)
+    try:
+        coding, _ = tokenize.detect_encoding(fd.readline)
+        fd.seek(0)
+        return io.TextIOWrapper(fd, coding).read()
+    except (LookupError, SyntaxError, UnicodeError):
+        return stdin_value.decode("utf-8")
 
 
 def config_parser(linenumber_config):
     size_pairs = map(lambda s: s.split('='), linenumber_config)
     return {Path(filename): int(size) for filename, size in size_pairs}
+
+def parse_unified_diff(diff: Optional[str] = None) -> Dict[str, Set[int]]:
+    """Parse the unified diff passed on stdin.
+
+    :returns:
+        dictionary mapping file names to sets of line numbers
+    """
+    # Allow us to not have to patch out stdin_get_value
+    if diff is None:
+        diff = stdin_get_value()
+
+    number_of_rows = None
+    current_path = None
+    parsed_paths: Dict[str, Set[int]] = collections.defaultdict(set)
+    for line in diff.splitlines():
+        if number_of_rows:
+            if not line or line[0] != "-":
+                number_of_rows -= 1
+            # We're in the part of the diff that has lines starting with +, -,
+            # and ' ' to show context and the changes made. We skip these
+            # because the information we care about is the filename and the
+            # range within it.
+            # When number_of_rows reaches 0, we will once again start
+            # searching for filenames and ranges.
+            continue
+
+        # NOTE(sigmavirus24): Diffs that we support look roughly like:
+        #    diff a/file.py b/file.py
+        #    ...
+        #    --- a/file.py
+        #    +++ b/file.py
+        # Below we're looking for that last line. Every diff tool that
+        # gives us this output may have additional information after
+        # ``b/file.py`` which it will separate with a \t, e.g.,
+        #    +++ b/file.py\t100644
+        # Which is an example that has the new file permissions/mode.
+        # In this case we only care about the file name.
+        if line[:3] == "+++":
+            current_path = line[4:].split("\t", 1)[0]
+            # NOTE(sigmavirus24): This check is for diff output from git.
+            if current_path[:2] == "b/":
+                current_path = current_path[2:]
+            # We don't need to do anything else. We have set up our local
+            # ``current_path`` variable. We can skip the rest of this loop.
+            # The next line we will see will give us the hung information
+            # which is in the next section of logic.
+            continue
+
+        hunk_match = DIFF_HUNK_REGEXP.match(line)
+        # NOTE(sigmavirus24): pep8/pycodestyle check for:
+        #    line[:3] == '@@ '
+        # But the DIFF_HUNK_REGEXP enforces that the line start with that
+        # So we can more simply check for a match instead of slicing and
+        # comparing.
+        if hunk_match:
+            (row, number_of_rows) = (
+                1 if not group else int(group) for group in hunk_match.groups()
+            )
+            assert current_path is not None
+            parsed_paths[current_path].update(range(row, row + number_of_rows))
+
+    # We have now parsed our diff into a dictionary that looks like:
+    #    {'file.py': set(range(10, 16), range(18, 20)), ...}
+    return parsed_paths
 
 
 class LineNumberPlugin:
